62318fb6ad03367d0071896a2fc0e1ab1c56fabd
Tiafeno <tiafenofnel@gmail.com>
2018-01-26 11:56:49 +0300

    Menu responsive

--- header.php
+++ header.php
@@ -99,94 +99,114 @@ global $MESSAGE;
       text-decoration: inherit;
       text-transform: none;
     }
+    #offcanvas-overlay .uk-active > a {
+      color: #fff !important;
+    }
   </style>
 
   </head>
 
 <body <?php body_class(); ?>>
-	<header>
-  	<div id="er-header-top" class="uk-content uk-content-small">
-      <div class="er-navbar-top">
+  <div class="uk-offcanvas-content">
+    <header>
+      <div id="er-header-top" class="uk-content uk-content-small">
+        <div class="er-navbar-top">
 
-        <?php if ( has_nav_menu( "top_menu" ) ) : ?>
-          <div class="uk-container uk-container-small">
-            <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
-            <?php
-              wp_nav_menu( [
-                'menu_class' => "uk-subnav uk-margin-remove",
-                'theme_location' => 'top_menu',
-                'container_class' => 'uk-navbar-right'
-              ] );
-            ?>
-            
-            </nav>
+          <?php if ( has_nav_menu( "top_menu" ) ) : ?>
+            <div class="uk-container uk-container-small">
+              <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+              <?php
+                wp_nav_menu( [
+                  'menu_class' => "uk-subnav uk-margin-remove",
+                  'theme_location' => 'top_menu',
+                  'container_class' => 'uk-navbar-right'
+                ] );
+              ?>
+              
+              </nav>
+            </div>
+          <?php endif; ?>
+
           </div>
-        <?php endif; ?>
-
-        </div>
-      <div class="uk-container ukt-container-small">
-        <div class="uk-margin-medium-bottom uk-margin-top" uk-grid>
-          <div class="uk-width-1-2 er-logo">
-            <!-- logo -->
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="uk-logo">
-              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.jpg" alt="<?php bloginfo( 'name' ); ?>" />
-            </a>
-          </div>	
+        <div class="uk-container ukt-container-small">
+          <div class="uk-margin-medium-bottom uk-margin-top" uk-grid>
+            <div class="uk-width-1-2 er-logo">
+              <!-- logo -->
+              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="uk-logo">
+                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.jpg" alt="<?php bloginfo( 'name' ); ?>" />
+              </a>
+            </div>	
+          </div>
         </div>
       </div>
-    </div>
-    <div id="er-header-nav" class="uk-container uk-container-small">
-      <!-- main navigation -->
-      <?php if ( has_nav_menu( 'primary' ) ) : ?>
+      <div id="er-header-nav" class="uk-container uk-container-small">
+        <!-- main navigation -->
+        <?php if ( has_nav_menu( 'primary' ) ) : ?>
 
-        <nav class="uk-navbar-container uk-navbar-transparent main-navigation" style="margin-bottom: 15px; margin-right: 9px" uk-navbar>
-          <div class="uk-margin-auto-left uk-flex">
-            <?php
-              wp_nav_menu( [
-                'menu_class' => "uk-subnav uk-subnav-pill er-subnav uk-margin er-navbar-right",
-                'theme_location' => 'primary',
-                'container_class' => 'uk-navbar-right',
-                'walker' => new Primary_Walker
-              ] );
+          <nav class="uk-navbar-container uk-navbar-transparent main-navigation" style="margin-bottom: 15px; margin-right: 9px" uk-navbar>
+            <div class="uk-margin-auto-left uk-flex">
+              <?php
+                wp_nav_menu( [
+                  'menu_class' => "uk-subnav uk-subnav-pill er-subnav uk-margin er-navbar-right uk-visible@m",
+                  'theme_location' => 'primary',
+                  'container_class' => 'uk-navbar-right',
+                  'walker' => new Primary_Walker
+                ] );
 
-              $usr = is_user_logged_in() ? wp_get_current_user() : 'Mon Compte';
-              $profil_page_id = get_option( "profil_page_id", false );
-              $profil_url = $usr instanceof WP_User ? get_the_permalink( (int)$profil_page_id ) : get_the_permalink( get_option( 'login_page_id' ) );
-              $profil_name = $usr instanceof WP_User ? $usr->display_name  : $usr;
-            ?>
+                $usr = is_user_logged_in() ? wp_get_current_user() : 'Mon Compte';
+                $profil_page_id = get_option( "profil_page_id", false );
+                $profil_url = $usr instanceof WP_User ? get_the_permalink( (int)$profil_page_id ) : get_the_permalink( get_option( 'login_page_id' ) );
+                $profil_name = $usr instanceof WP_User ? $usr->display_name  : $usr;
+              ?>
 
-            <div class="uk-navbar-right er-navbar-right">
-              <div class=" ">
-                <ul id="menu-compte" class="uk-subnav uk-subnav-pill er-user-subnav uk-margin">
-                  <li>
-                    <a href='<?= $profil_url ?>'><span class='er-user-icon' uk-icon='icon: user'></span><span class='er-user-text'><?= $profil_name ?></span></a> 
-                  </li>
-                </ul>
+              <div class="uk-navbar-right uk-hidden@m">
+                <button class="uk-button uk-button-default" type="button" uk-toggle="target: #offcanvas-overlay">MENU</button>
+                <div id="offcanvas-overlay" uk-offcanvas="overlay: true; mode: push">
+                
+                  <?php
+                  wp_nav_menu( [
+                    'menu_class' => "uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical",
+                    'theme_location' => 'primary',
+                    'container_class' => 'uk-offcanvas-bar uk-flex uk-flex-column',
+                    'walker' => new Primary_offcanvas_Walker
+                  ] );
+                  ?>
+
+                </div>
+              </div>
+
+              <div class="uk-navbar-right er-navbar-right">
+                <div class=" ">
+                  <ul id="menu-compte" class="uk-subnav uk-subnav-pill er-user-subnav uk-margin">
+                    <li>
+                      <a href='<?= $profil_url ?>'><span class='er-user-icon' uk-icon='icon: user'></span><span class='er-user-text'><?= $profil_name ?></span></a> 
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+
+        <?php endif; ?>
+      </div>
+    </header>
+    <div id="primary">
+    
+      <?php 
+          if ( is_active_sidebar( 'middle-area' ) ) : 
+            dynamic_sidebar( 'middle-area' ); 
+          endif; 
+        ?>
+      <?php if ( ! is_null($MESSAGE)) : ?>
+          <div class="uk-section uk-section-large uk-padding-small ">
+            <div class="uk-container uk-container-small">
+              <div class="ui <?= $MESSAGE->type ?> message">
+                <div class="header">
+                <?= $MESSAGE->get_title() ?>
+                </div>
+                <p><?= $MESSAGE->get_message() ?></p>
               </div>
             </div>
           </div>
-        </nav>
-
       <?php endif; ?>
-    </div>
-  </header>
-  <div id="primary">
-  
-    <?php 
-        if ( is_active_sidebar( 'middle-area' ) ) : 
-          dynamic_sidebar( 'middle-area' ); 
-        endif; 
-      ?>
-    <?php if ( ! is_null($MESSAGE)) : ?>
-        <div class="uk-section uk-section-large uk-padding-small ">
-          <div class="uk-container uk-container-small">
-            <div class="ui <?= $MESSAGE->type ?> message">
-              <div class="header">
-              <?= $MESSAGE->get_title() ?>
-              </div>
-              <p><?= $MESSAGE->get_message() ?></p>
-            </div>
-          </div>
-        </div>
-    <?php endif; ?>
     
