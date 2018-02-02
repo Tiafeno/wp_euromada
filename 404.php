77a7e9c00dd01f72d78bf617c30c61366d179940
Tiafeno <tiafenofnel@gmail.com>
2018-02-02 19:56:30 +0300

    Content 404 page error

---
+++ 404.php
@@ -0,0 +1,27 @@
+
+<?php
+/**
+ * The template for displaying error 404 
+ *
+ * @package Tiafeno
+ * @subpackage wp_euromada
+ * @since Euromada 1.0
+ */
+
+get_header(); 
+?>
+
+      <div id="primary-content">
+        <div class="uk-section uk-section-large uk-padding-medium">
+          <div class="uk-container uk-container-small">
+            <h2>This is somewhat embarrassing, isn’t it?</h2>
+            <p>It looks like nothing was found at this location. Maybe try a search?</p>
+          </div>
+        </div>
+      </div>
+      <?php get_footer(); ?>
+    </div>
+    <?php wp_footer(); ?>
+  </div>
+</body>
+</html>
